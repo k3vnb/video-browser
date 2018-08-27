94d03464ae71ca5d18d534cecb11c2f4092ff0ed
Kevin Boyle <papershack@gmail.com>
2018-08-27 01:07:58 -0700

    wire up first component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
+    <SearchBar></SearchBar>
     Hello from Vue video-browser app
   </div>
 </template>
 
 <script>
+  import SearchBar from './components/SearchBar';
   export default {
     name: 'App',
+    components: {
+      //es6 says, since the keyvalue pairs are identical, you can just write SearchBar, but for clarity sake I included both key & value
+      SearchBar: SearchBar
+    }
   };
 </script>
