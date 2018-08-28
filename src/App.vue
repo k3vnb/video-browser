c801029002846094174f77a9c7ad53b940e88599
Kevin Boyle <papershack@gmail.com>
2018-08-28 16:44:52 -0700

    set up structure for passing props to child component (VideoList) from parent (App)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <template>
   <div>
     <SearchBar v-on:termChange="onTermChange"></SearchBar>
-    Hello from Vue video-browser app
+    <VideoList v-bind:videos="videos"></VideoList>
   </div>
-  <!-- it is also ok to write as <SearchBar @termChange="onTermChange"> -->
+  <!-- it is also ok to write as <SearchBar @termChange="onTermChange">... v-bind:videos="videos" can also be written as :videos="videos" -->
+  <!-- <VideoList v-bind:videos="videos">.. the left 'videos' is what we are now calling the props we're passing down. The right 'videos' refers directly to the data property videos from our script -->
 </template>
 
 <script>
   import axios from 'axios';
   import SearchBar from './components/SearchBar';
-  const API_KEY = require('./../env').apiKey;;
+  import VideoList from './components/VideoList';
+  const API_KEY = require('./../env').apiKey;
   //input api key above;
 
   export default {
     name: 'App',
     components: {
       //es6 says, since the keyvalue pairs are identical, you can just write SearchBar, but for clarity sake I included both key & value
-      SearchBar: SearchBar
+      SearchBar: SearchBar,
+      VideoList: VideoList
+    },
+    // data properties are where you want the View to update based on new data, ie {{ videos.length }} in template refers to data's video property
+    data: function(){
+      return{
+        videos: []
+       };
     },
     methods: {
       onTermChange(searchTerm){
@@ -27,7 +36,9 @@
             part: 'snippet',
             q: searchTerm
           }
-        }).then(response => console.log(response));
+        }).then(response => {
+          this.videos = response.data.items
+        });
       }
       // can also be seen as onTermChange: function(searchTerm){};
       // searchTerm is the second arg from onInput, aka event.target.value;
---
+++ src/components/VideoList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    VideoList
+    {{ videos.length }}
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'VideoList',
+  props: ['videos']
+}
+</script>
+
+<style>
+</style>
