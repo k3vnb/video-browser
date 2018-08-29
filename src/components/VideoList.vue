c491a1fd4eea2e9cc5b0fd4ed614c7edec11c20a
Kevin Boyle <papershack@gmail.com>
2018-08-28 17:00:35 -0700

    add new component and for-loop to render props from parent to child component

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,13 +1,23 @@
 <template>
   <ul>
-    VideoList
-    {{ videos.length }}
+    <VideoListItem
+      v-for="video in videos"
+      v-bind:video="video"
+      :key="video.etag"
+      >
+    </VideoListItem>
+      <!-- above, v-bind:video="video", the right "video" refers to the 'video' in our v-for loop, not the props below -->
+      <!-- etag is an identitifying key provided by Youtube -->
   </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem';
 export default {
   name: 'VideoList',
+  components: {
+    VideoListItem: VideoListItem
+  },
   props: ['videos']
 }
 </script>
